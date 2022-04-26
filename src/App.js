c6016b937a311abd80239093752cbfedfd3037c0
Toussaint L Turnier <toussaintturnier98@gmail.com>
2022-04-26 14:09:57 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StarRating from './StarRating';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <StarRating/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StarRating.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {FaStar} from "react-icons/fa";
+
+const StarRating = ()=>{
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+    return(
+        <div>
+        {[...Array(5)].map((star, i) => {
+            const ratingValue = i + 1;
+            return(
+                <label>
+                <input
+                type="radio"
+                name="rating"
+                onClick={()=>setRating(ratingValue)}
+                />
+                <FaStar
+                className="star"
+                color={ratingValue<=(hover || rating) ? "#ffc107" : "#e4e5e9"}
+                size={100}
+                onMouseEnter={()=>setHover(ratingValue)}
+                onMouseLeave={()=>setHover(null)}
+                />
+                </label>
+            );
+        })}</div>
+    )
+};
+
+
+export default StarRating;
